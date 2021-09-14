40caaf7424bcd63a02881665f95ae49bc80ab3fb
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-09-14 10:01:30 +0300

    Add Buttons

--- src/components/buttons/Buttons.jsx
+++ src/components/buttons/Buttons.jsx
@@ -2,29 +2,48 @@ import React from "react";
 import Button from "@mui/material/Button";
 import SaveIcon from "@mui/icons-material/Save";
 import DeleteIcon from "@mui/icons-material/Delete";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+import { green, grey, orange } from "@mui/material/colors";
 
 function Buttons() {
     return (
-        <div>
-            <Button
-                variant="contained"
-                color="secondary"
-                size="large"
-                startIcon={<SaveIcon />}
-            >
+        <ButtonGroup variant="contained" size="large">
+            <Button color="secondary" startIcon={<SaveIcon />}>
                 SAVE
             </Button>
 
-            <Button
-                variant="contained"
-                color="error"
-                size="large"
-                endIcon={<DeleteIcon />}
-            >
+            <Button color="error" endIcon={<DeleteIcon />}>
                 DELETE
             </Button>
-        </div>
+        </ButtonGroup>
     );
 }
 
-export default Buttons;
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: green[500],
+            contrastText: grey[50],
+        },
+        secondary: orange,
+    },
+});
+
+function Buttons2() {
+    return (
+        <ThemeProvider theme={theme}>
+            <ButtonGroup variant="contained" size="large">
+                <Button color="secondary" startIcon={<SaveIcon />}>
+                    SAVE
+                </Button>
+
+                <Button color="primary" endIcon={<DeleteIcon />}>
+                    DELETE
+                </Button>
+            </ButtonGroup>
+        </ThemeProvider>
+    );
+}
+
+export { Buttons, Buttons2 };
